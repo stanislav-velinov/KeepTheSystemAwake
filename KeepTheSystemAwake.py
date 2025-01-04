a2085d1d7784008e4aaefa29d404799c82f7ff35
Stanislav Velinov <106009997+stanislav-velinov@users.noreply.github.com>
2025-01-04 11:52:51 +0200

    Adding the .py file + .exe file
    Adding both the .py file
    and the program itself via .exe file

---
+++ KeepTheSystemAwake.py
@@ -0,0 +1,23 @@
+import ctypes
+import time
+
+
+# Simulate a key press to keep the computer awake
+def press_key():
+    ctypes.windll.user32.keybd_event(0x90, 0, 0, 0)  # Simulate pressing the 'Num Lock' key
+    time.sleep(1)  # Small delay
+    ctypes.windll.user32.keybd_event(0x90, 0, 2, 0)  # Release the 'Num Lock' key
+
+
+def keep_awake():
+    print("Keeping the system awake. Press Ctrl+C to stop.")
+    try:
+        while True:
+            press_key()
+            time.sleep(30)  # Wait 1 minute before simulating the next key press
+    except KeyboardInterrupt:
+        print("Program stopped.")
+
+
+if __name__ == "__main__":
+    keep_awake()
